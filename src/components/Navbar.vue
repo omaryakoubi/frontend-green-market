c72d9f2b407293088f3a3f80d30d12f465912871
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-26 11:27:32 +0100

    implementig the navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">Green</span>
+        <span>Market</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn falt color="grey">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped></style>
