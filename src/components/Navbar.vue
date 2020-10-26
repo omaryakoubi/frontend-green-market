23ab4e17ea552f6ac98ad914d9ab89b14859afcd
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-26 13:09:35 +0100

    advancing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase">
         <span class="font-weight-light">Green</span>
         <span>Market</span>
@@ -11,12 +12,21 @@
         <v-icon right>mdi-exit-to-app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="green">
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
   name: "Navbar",
+  data() {
+    return {
+      drawer: false
+    };
+  },
+
 };
 </script>
 
