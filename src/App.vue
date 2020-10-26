e973fe8bb76d3765dfd517e0bd0520277216f64a
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-26 10:31:22 +0100

    restarting

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <Productform />
   </div>
 </template>
 
 <script>
-import Productform from "./components/Product-form.vue";
 export default {
   name: "App",
-  components: { Productform },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from "vue";
-import Vuesax from "vuesax";
 import App from "./App.vue";
-import "vuesax/dist/vuesax.css"; //Vuesax styles
 
 Vue.config.productionTip = false;
 
-Vue.use(Vuesax);
 
 new Vue({
   render: (h) => h(App),
